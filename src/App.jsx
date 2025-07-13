3a7545cf72a3601f29302620490472d918f458b2
Subrata Guchhait <sguchhaitdev@gmail.com>
2025-07-13 22:45:17 +0530

    Implement Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+
 function App() {
   return (
     <>
-      <h1>Devtinder</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          {/* <Route path="/profile" element={<div>Profile Page</div>} />
+          <Route path="/connections" element={<div>Connection Page</div>} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
