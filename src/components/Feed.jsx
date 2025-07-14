043aae41131933f745b87f19127207817d405cc4
Subrata Guchhait <sguchhaitdev@gmail.com>
2025-07-14 14:00:43 +0530

    Build user csrd on feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import React, { useEffect } from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const feed = useSelector((store) => store?.feed);
+  const dispatch = useDispatch();
+  const getFeedData = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      console.log(res);
+      dispatch(addFeed(res?.data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getFeedData();
+  }, []);
+  return (
+    <div className="flex justify-center pt-2">
+      {feed && <UserCard user={feed[0]} />}
+    </div>
+  );
 };
 
 export default Feed;
